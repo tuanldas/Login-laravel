b6dea51ffd1132e5ad6ef399eb37de373d367cb9
tuan <tuan.le@codegym.vn>
2021-08-18 12:03:10 +0700

    first commit

---
+++ resources/views/welcome_admin.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Chào mừng admin</title>
+</head>
+<body>
+<p>Chào mừng admin đến với trang web.</p>
+</body>
+</html>
+
